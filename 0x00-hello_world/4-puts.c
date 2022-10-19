3acdfd6d6538784632cd55b26f78de8aca9a9287
filimonkd <filimonkd@gmail.com>
2022-10-19 07:12:38 -0700

    fix commenting error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-
 {
-
-	char hello[] = "\"Programming is like building a multilingual puzzle";
-
-
-
-	puts(hello);
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
